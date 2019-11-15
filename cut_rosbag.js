ddb9c42c96dc784db20af27f340515cb3c911098
Hayato Mizushima <hayato.mizushima@tier4.jp>
2019-11-15 15:58:08 +0900

    ファイルとフィルタ条件をクエリストリングにした。

--- cut_rosbag.js
+++ cut_rosbag.js
@@ -1,17 +1,42 @@
 const fs = require('fs');
 const http = require('http');
+const urlParser = require('url');
+const qs = require('querystring'); 
 
 const {execSync} = require('child_process');
 
 let server = http.createServer(
     (request, response)=>{
-        execSync('rosbag filter 2011-01-19-07-49-38.bag filter.bag "t.secs <= 1295452260"');
-        console.log("finished filter")
-        var filename = "./filter.bag";
-        var raw = fs.createReadStream( filename );
-        var resHeader = {
+        const url = urlParser.parse(request.url);
+        const queryString = qs.parse(url.query);
+        const inFile = queryString.inFile;
+        const startTime = queryString.startTime;
+        const endTime = queryString.endTime;
+        let outFile = inFile;
+        let bashCommand = "echo filter bag";
+
+        if (startTime !== undefined && endTime !== undefined){
+            outFile = "filtered_from_"+ startTime +"_to_" + endTime + inFile
+            bashCommand = "rosbag filter " + inFile + " " + outFile + " 't.secs >= " + startTime + " and t.secs <= " + endTime + "'";
+        }
+
+        if (startTime !== undefined && endTime === undefined){
+            outFile = "filtered_from_"+ startTime + "_" + inFile
+            bashCommand = "rosbag filter " + inFile + " " + outFile + " 't.secs >= " + startTime + "'";
+        }
+
+        if (startTime === undefined && endTime !== undefined){
+            outFile = "filtered_to_"+ endTime + "_" + inFile
+            bashCommand = "rosbag filter " + inFile + " " + outFile + " 't.secs <= " + endTime + "'";
+        }
+
+        execSync(bashCommand);
+        console.log("finished filterring");
+
+        const raw = fs.createReadStream( outFile );
+        const resHeader = {
             'Content-Type' : 'application/rosbag',
-            'Content-Disposition' : 'attachment; filename="filter.bag"'
+            'Content-Disposition' : 'attachment; filename='+ outFile
         };
         response.writeHead(200, resHeader );
         raw.pipe(response);
