ce9fd91e09433190157651469eeb094f28c6f0b7
Hayato Mizushima <hayato.mizushima@tier4.jp>
2019-11-18 15:33:14 +0900

    topicのフィルタ条件が未指定の場合の処理を追加

--- node_rosbag_filter.js
+++ node_rosbag_filter.js
@@ -9,12 +9,13 @@ let server = http.createServer(
     (request, response)=>{
         const url = urlParser.parse(request.url);
         const queryString = qs.parse(url.query);
-        const inFile = queryString.inFile;
+        const inBag = queryString.inBag;
         const startTime = queryString.startTime;
         const endTime = queryString.endTime;
-        const topics = queryString.topics.split(','); //,区切りで複数個渡す
-        let outFile = "filtered_" + inFile;
-        let bashCommand = "rosbag filter " + inFile + " " + outFile + " '";
+        const topics_str = queryString.topics; //,区切り
+        const topics = (topics_str !== undefined) ? topics_str.split(',') : undefined;
+        let outBag = "filtered_" + inBag;
+        let bashCommand = "rosbag filter " + inBag + " " + outBag + " '";
 
         if(topics !== undefined){
             for(var i=0; i<topics.length; i++){
@@ -45,10 +46,10 @@ let server = http.createServer(
         execSync(bashCommand);
         console.log("finished filterring");
 
-        const raw = fs.createReadStream( outFile );
+        const raw = fs.createReadStream( outBag );
         const resHeader = {
             'Content-Type' : 'application/rosbag',
-            'Content-Disposition' : 'attachment; filename='+ outFile
+            'Content-Disposition' : 'attachment; filename='+ outBag
         };
         response.writeHead(200, resHeader );
         raw.pipe(response);
